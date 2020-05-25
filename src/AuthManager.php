ac7d32fd788b9091750a5c6ed4b21190f5c41398
Danut Florian <daniesy@me.com>
2020-05-25 12:28:22 +0300

    Added support for custom authenticator

---
+++ src/AuthManager.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Daniesy\Rodels;
+
+
+use Daniesy\Rodels\Auth\KeyAuthenticator;
+use Illuminate\Support\Manager;
+
+class AuthManager extends Manager
+{
+
+    public function createKeyDriver()
+    {
+        return new KeyAuthenticator;
+    }
+
+    /**
+     * Get the default mail driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+        return $this->app['config']['rodels.auth'];
+    }
+}
