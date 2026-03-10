0cbf9e63a92852997529edb033d76ebd7496fba4
Dan Florian <dan@attacksimulator.com>
2026-03-10 14:13:15 +0200

    Normalized invalid HTTP status codes to 500 in ApiException
    When curl fails with connection errors (refused, timeout), the HTTP code
    is 0. Passing this to Symfony's HttpException crashes the response
    renderer since 0 is not a valid HTTP status code.

--- src/Api/Exceptions/ApiException.php
+++ src/Api/Exceptions/ApiException.php
@@ -8,6 +8,7 @@ class ApiException extends HttpException
 {
     public function __construct(int $statusCode, array $messages = [], \Throwable $previous = null, array $headers = [], ?int $code = 0)
     {
+        $statusCode = ($statusCode >= 100 && $statusCode < 600) ? $statusCode : 500;
         parent::__construct($statusCode, json_encode($messages), $previous, $headers, $code);
     }
 }
